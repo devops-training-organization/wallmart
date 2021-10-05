cfdc2a52beb3cd759f9bb0763351289e8f2f2d5d
sachdevdhruv <dhruvsachdev23@gmail.com>
2021-10-05 12:49:50 -0400

    multi merge fixed

--- DBUtils.java
+++ DBUtils.java
@@ -1,3 +1,2 @@
 Here I am updating in Dev branch
-Hello
 Updatiing file and going to use -a to commit
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 Updating for stash command
 Hello I am updating with developemnt branch
-<<<<<<< HEAD
-this is third tes
-=======
 I am updating this file 
->>>>>>> development
